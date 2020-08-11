1af303657f9abd5b6cab8cddc697cccf19d7db68
Debashish Palit <dpalit07@gmail.com>
2020-08-11 20:44:12 +0530

    mobile-drag-drop polyfill

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import UUID from 'vue-uuid'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { polyfill } from 'mobile-drag-drop'
+import { scrollBehaviourDragImageTranslateOverride } from 'mobile-drag-drop/scroll-behaviour'
+
+polyfill({
+  dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+})
 
 Vue.use(VueDraggable)
 Vue.use(BootstrapVue)
